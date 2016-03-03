a0dc55b2c868f138782400eccb4234c9d3244c99
Sergio <Sergio@Sergio-PC>
2016-03-03 17:10:17 +0100

    commit

--- pruebaGit/src/pruebaGit/Principal.java
+++ pruebaGit/src/pruebaGit/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("buenos dias soy sergio");
+		System.out.println("buenos dias soy sergio 2");
 		
 	}
 
